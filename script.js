877170d24585dbe05981b143d968cbff4efa55a3
Aviral Srivastava <aviralji4@gmail.com>
2022-01-12 22:06:47 +0530

    Added decryption functionality

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ function savecipher() {
     saveAs(blob, "secret.txt");
 
 }
+function decryptcipher() {
+    document.getElementById('inputfile').addEventListener('change', function () {
+        var fr = new FileReader();
+        fr.onload = function () {
+            var decrypted = CryptoJS.enc.Utf8.stringify(CryptoJS.enc.Base64.parse(fr.result));
+            alert("Your message says: " + decrypted);
+        }
+        fr.readAsText(this.files[0]);
+    });
+}
